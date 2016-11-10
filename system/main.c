2de91ddbceb2e41c5bc9a8144b333e279cc60e3c
Christopher Waugh <kenkron@gmail.com>
2016-11-10 09:55:50 -0500

    Patch applied. Main modified to contain a simple program.
    Program is a two process hello world that demonstrates an mprintf, which will ensure that two printf commands to not occur at the same time

--- system/main.c
+++ system/main.c
@@ -1,23 +1,49 @@
-/*  main.c  - main */
+/*  main.c  - main processSender processReceiver processAdditionalReceiver*/
 
 #include <xinu.h>
 
+#define OE_ADDR 0x134
+#define GPIO_DATAOUT 0x13C
+#define GPIO_DATAIN 0x138
+#define GPIO0_ADDR 0x44E07000
+#define GPIO1_ADDR 0x4804C000
+#define GPIO2_ADDR 0x481AC000
+#define GPIO3_ADDR 0x481AF000
+
+pid32 FOO;
+pid32 BAR;
+
+/* This prevents the processes from cutting each other off when printing*/
+sid32 printMutex;
+#define mprintf(...) wait(printMutex);kprintf (__VA_ARGS__);signal(printMutex)
+
+process foo(){
+	mprintf("%d foo\n", getpid());
+}
+
+process bar(){
+	mprintf("%d bar\n", getpid());
+}
+
+/* starts a sender and three receivers that dance around sending 
+ * each other messages until they figure they've run through everything.
+ * The sender usually sends things, but it technically gets sent
+ * messages by receivers 2 and 3.  Otherwise, how would we know if
+ * buffers for old message queues could be recycled.*/
 process	main(void)
 {
-
-	/* Run the Xinu shell */
-
+	printMutex=semcreate(1);
+	mprintf("%d STARTING\n\n", getpid());
+	
 	recvclr();
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	while (TRUE) {
-		receive();
-		sleepms(200);
-		kprintf("\n\nMain process recreating shell\n\n");
-		resume(create(shell, 4096, 20, "shell", 1, CONSOLE));
-	}
+	FOO = create(foo, 4096, 50, "foo", 0);
+	BAR = create(bar, 4096, 50, "bar", 0);
+	
+	resched_cntl(DEFER_START);
+	resume(FOO);
+	resume(BAR);
+	resched_cntl(DEFER_STOP);
+	
+	mprintf("\n%d DONE\n", getpid());
 	return OK;
-    
 }
