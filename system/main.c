1a0cc59a187e1a4f787b1d5a6514850e8c72ef66
Nick Jiang <njiang1209@ufl.edu>
2016-11-22 18:15:19 -0500

    push ban main

--- system/main.c
+++ system/main.c
@@ -8,82 +8,39 @@ extern syscall turn_light_off(did32);
 extern syscall turn_light_on(did32);
 
 local process pushbtn(void);
-local process status_print(void);
-local process led(void);
 
-sid32 psem; /*print sem*/
+#ifndef BTN_JSON
+#define BTN_JSON "{\"name\" : \"push_btn\", \"value\": \"1\"}"
+#endif
+
+MQTTSN_topicid topic;
 
 /*NOTE: Process checks to see if button is pressed. Checks two functions: if_pressed() and button_status()*/
 process pushbtn(void) {
-	
 	/*PUSHBTN=push button, found in config/config.h */
 	while(TRUE) {
-#if 1
 		if (if_pressed((did32) PUSHBTN)) {
-			wait(psem);
-			kprintf("PUSHBTN pushed!\n");
-			uint32 btn_status = button_status((did32) PUSHBTN);
-			kprintf("PUSHBTN status: %d\n",btn_status);
-			signal(psem);
+			char payload[] = BTN_JSON;
+			mqttsn_publish (topic, BTN_JSON, 35);
+			sleep(2);
 		}
-#endif
 	}
 	return OK;
 }
 
-/*NOTE: Process checks to make sure button_status() will output '0' if btn NOT pressed*/
-process status_print(void){
-	while(TRUE) {
-		sleep(3);
-		uint32 btn_status = button_status((did32) PUSHBTN);
-		wait(psem);
-		kprintf("PUSHBTN status: %d\n",btn_status);
-		signal(psem);
-	}
-	return OK;
-}
-
-
-/*NOTE: Process checks turn_light_off() and turn_light_on() and light_status()*/
-process	led(void)
-{
-	/*LED=led, found in config/config.h */
-	while(TRUE) {
-		sleep(1);
-		turn_light_off((did32) LED);
-		wait(psem);
-		kprintf("Light Status: %d\n",light_status((did32) LED));
-		signal(psem);
-		sleep(1); /*sleep 2 seconds*/
-		turn_light_on((did32) LED);
-		wait(psem);
-		kprintf("Light Status: %d\n",light_status((did32) LED));
-		signal(psem);
-	}
-
-	return OK;
-}
-
-void handler2(MQTTSN_topicid topic, unsigned char * data, int32 datalen)
-{
-	kprintf("GOT SOME DATA!!!! WITH LEN: %d\n", datalen);
-	return;
-}
 
 process main(void) {
+	unsigned char data[2];
+	topic.type = 0;
+	topic.data.long_.name = "home/bedroom/btn";
+	topic.data.long_.len = 16;
 
 	recvclr();
-	kprintf("Starting LED and PUSHBTN demo...\n");
-	
-	psem = semcreate(1);
-	pid32 led_id = create(led,4096,50,"led",0);
+
 	pid32 btn_id = create(pushbtn,4096,50,"btn",0);
-	pid32 status_id = create(status_print,4096,50,"status",0);
 
 	resched_cntl(DEFER_START);
-	resume(led_id);
 	resume(btn_id);
-	resume(status_id);
 	resched_cntl(DEFER_STOP);
 }
 
