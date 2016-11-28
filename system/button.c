f2588696065b36d0acac9f28fb03aba44f35098e
Nick Jiang <njiang1209@ufl.edu>
2016-11-28 11:27:35 -0500

    wrappers

--- system/button.c
+++ system/button.c
@@ -3,16 +3,16 @@
 #include <xinu.h>
 
 syscall button_status(did32 dev_id){
-	return gpioingetc(&devtab[dev_id]);
+	return getc(&devtab[dev_id]);
 };
 
 
 syscall if_pressed(did32 dev_id){
-	return gpioingetc(&devtab[dev_id]);
+	return getc(&devtab[dev_id]);
 };
 
 /*Note I do not think this works as intended. I think when you write a '1' to DATAIN. The register gets rewritten at next time clock. This makes sense because if you click a button that does not stay down, it will only register once*/
 syscall virtual_button_press(did32 dev_id,bool8 engage){
-	return gpioinputc(&devtab[dev_id],engage);
+	return putc(&devtab[dev_id],engage);
 };
 
