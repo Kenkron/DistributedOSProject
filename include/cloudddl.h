018d40bfece6f36c82942af1c55e2c32440da6d1
Christopher Waugh <kenkron@gmail.com>
2016-11-28 16:19:44 -0500

    changing UUID

--- include/cloudddl.h
+++ include/cloudddl.h
@@ -1,4 +1,4 @@
-#define DEVICE_UUID "9FBE1B9C-D129-4F62-A33E-6B20B09C34F6"
+#define DEVICE_UUID "088881b6-a2a5-48f7-85c2-820880204b04"
 #define DEVICE_UUID_LENGTH 36
-#define DEVICE_DESCRIPTION "{\"type\": \"button\", \"publishes\": [ \"#/force-update\" ], \"subscribes\": [], \"description\": \"A simple publish-only button that forces any subscribed sensors tosend updated data.\"}"
+#define DEVICE_DESCRIPTION "{\"type\": \"tempat\", \"publishes\": [ \"#/force-update\" ], \"subscribes\": [], \"description\": \"A simple publish-only button that forces any subscribed sensors tosend updated data.\"}"
 #define DEVICE_DESCRIPTION_LENGTH 188
