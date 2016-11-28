68fa676702a110fe09c01fe54444be6b2bfd435c
drew <gonsalvd@gmail.com>
2016-11-28 13:21:16 -0500

    Added led uuid,etc clouddl.h

---
+++ include/clouddl.h
@@ -0,0 +1,4 @@
+#define DEVICE_UUID "F09AF8A3-4828-4D0C-BA3F-947FAD1779F7"
+#define DEVICE_UUID_LENGTH 36
+#define DEVICE_DESCRIPTION "{\"type\": \"led\",\"publishes\": [],\"subscribes\": [\"#/temperature\"],\"description\": \"An LED that lights up when a temperature reading is sent.\"}"
+#define DEVICE_DESCRIPTION_LENGTH strlen(DEVICE_DESCRIPTION)
